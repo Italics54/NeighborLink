cb7afce7f27d1f0466c4597437eb28a7ef1f845a
Italics54 <nouryyj1028@gmail.com>
2025-11-09 18:15:01 -0600

    commit home

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('NeighborLink');
+  constructor(private router: Router) {}
+
+  openResources(event: Event) {
+    event.preventDefault(); 
+    const url = this.router.serializeUrl(
+      this.router.createUrlTree(['/resources'])
+    );
+    window.open(url, '_blank');
+  }
 }
