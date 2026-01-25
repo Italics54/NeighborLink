5c87fa52e94b3e10ef35748722cd5ca801fc3424
Italics54 <nouryyj1028@gmail.com>
2026-01-25 10:26:45 -0600

    Calender frontend

--- src/app/calendar/calendar.ts
+++ src/app/calendar/calendar.ts
@@ -1,9 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './calendar.html',
   styleUrl: './calendar.css'
 })
-export class Calendar {}
+export class Calendar {
+  getDay(date: string): string {
+    return new Date(date).getDate().toString();
+}
+  getMonth(date: string): string {
+    return new Date(date).toLocaleString('default', { month: 'short' });
+}
+
+
+  upcomingEvents = [
+    {
+      title: 'Community Potluck Dinner',
+      date: 'January 25, 2067',
+      time: '5:00 PM',
+      category: 'Gathering'
+    },
+    {
+      title: 'Coffee & Conversation',
+      date: 'January 26, 2067',
+      time: '9:00 AM',
+      category: 'Social'
+    },
+    {
+      title: 'Kids Art & Craft Hour',
+      date: 'January 27, 2067',
+      time: '3:00 PM',
+      category: 'Family'
+    }
+  ];
+}
