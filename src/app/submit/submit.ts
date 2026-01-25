53b45cedb5b070ba93c553aa7bf750bd55c398a7
Italics54 <nouryyj1028@gmail.com>
2026-01-25 14:55:39 -0600

    submit done

--- src/app/submit/submit.ts
+++ src/app/submit/submit.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-submit',
   standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './submit.html',
   styleUrl: './submit.css'
 })
 export class Submit {
+  submitted = false;
 
+  onSubmit() {
+    this.submitted = true;
+  }
+
+  resetForm() {
+    this.submitted = false;
+  }
 }
