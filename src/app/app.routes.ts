17049716129bfee7d08544a7e2b57a35a90ce6db
Zephan Ofori <ofori.zephan@gmail.com>
2026-01-24 18:33:27 -0600

    fixing calendar spelling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { Resources } from './resources/resources';
 import { Stories } from './stories/stories';
-import { Calender } from './calender/calender';
+import { Calendar } from './calendar/calendar';
 import { Submit } from './submit/submit';
 
 export const routes = [
   { path: '', component: HomeComponent },
   { path: 'resources', component: Resources },
   { path: 'stories', component: Stories },
-  { path: 'calender', component: Calender },
+  { path: 'calendar', component: Calendar },
   { path: 'submit', component: Submit },
 ];
--- src/app/calendar/calendar.spec.ts
+++ src/app/calendar/calendar.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Calender } from './calender';
+import { Calendar } from './calendar';
 
-describe('Calender', () => {
-  let component: Calender;
-  let fixture: ComponentFixture<Calender>;
+describe('Calendar', () => {
+  let component: Calendar;
+  let fixture: ComponentFixture<Calendar>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Calender]
+      imports: [Calendar]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Calender);
+    fixture = TestBed.createComponent(Calendar);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
