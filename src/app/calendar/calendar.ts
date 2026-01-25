2352fe19ecb38eec1b8941cfc337dafcfaac11d9
Zephan Ofori <ofori.zephan@gmail.com>
2026-01-25 14:51:38 -0600

    modal for calendar (needs css editing)

--- src/app/calendar/calendar.ts
+++ src/app/calendar/calendar.ts
@@ -11,10 +11,10 @@ import { CommonModule } from '@angular/common';
 export class Calendar {
   getDay(date: string): string {
     return new Date(date).getDate().toString();
-}
+  }
   getMonth(date: string): string {
     return new Date(date).toLocaleString('default', { month: 'short' });
-}
+  }
 
 
   upcomingEvents = [
@@ -35,6 +35,22 @@ export class Calendar {
       date: 'January 27, 2067',
       time: '3:00 PM',
       category: 'Family'
-    } 
+    }
   ];
+
+  selectedDay: number | null = null;
+  selectedEvents: any[] = [];
+
+  openDay(day: number) {
+    this.selectedDay = day;
+
+    this.selectedEvents = this.upcomingEvents.filter(
+      e => new Date(e.date).getDate() === day
+    );
+  }
+
+  closeModal() {
+    this.selectedDay = null;
+  }
+
 }
