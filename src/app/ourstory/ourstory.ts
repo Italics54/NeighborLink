47f249694697915e7b1f752e7bc7576c10daa7b0
Italics54 <nouryyj1028@gmail.com>
2026-01-27 15:47:33 -0600

    routing & styling

--- src/app/ourstory/ourstory.ts
+++ src/app/ourstory/ourstory.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ourstory',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   standalone: true,
   templateUrl: './ourstory.html',
   styleUrl: './ourstory.css',
 })
 export class Ourstory {
+    isLoggedIn = false;
+  username: string | null = null;
+    constructor(private router: Router, private authService: AuthService) {
+    this.authService.loggedIn$.subscribe(status => this.isLoggedIn = status);
+    this.authService.username$.subscribe(name => this.username = name);
+  }
 
+  goToSignUpDashboard() {
+    this.router.navigate(['/sign-up']);
+  }
 }
