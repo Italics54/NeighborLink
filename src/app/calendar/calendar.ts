ac67d2ce2b2221cc50ff5f2db5bf2eecf6c3ec83
Zephan Ofori <ofori.zephan@gmail.com>
2026-01-27 12:54:42 -0600

    updating calendar stuff

--- src/app/calendar/calendar.ts
+++ src/app/calendar/calendar.ts
@@ -20,21 +20,62 @@ export class Calendar {
   upcomingEvents = [
     {
       title: 'Community Potluck Dinner',
-      date: 'January 25, 2067',
+      date: 'February 7, 2026',
       time: '5:00 PM',
-      category: 'Gathering'
+      category: 'Gathering',
+      location: 'Harmony House Kitchen'
     },
     {
       title: 'Coffee & Conversation',
-      date: 'January 26, 2067',
+      date: 'February 16, 2026',
       time: '9:00 AM',
-      category: 'Social'
+      category: 'Social',
+      location: 'Unity Grove Park'
     },
     {
       title: 'Kids Art & Craft Hour',
-      date: 'January 27, 2067',
+      date: 'February 19, 2026',
       time: '3:00 PM',
-      category: 'Family'
+      category: 'Family',
+      location: 'Harmony Public Library'
+    },
+    {
+      title: 'Empty Plate Benefit Lunch',
+      date: 'February 25, 2026',
+      time: '3:00 PM',
+      category: 'Food Bank Event',
+      location: 'Harmony Point Whole Health'
+    }
+  ];
+
+  todaysEvents = [
+    {
+      title: 'Volunteer Shift: Farmers Market',
+      date: 'February 14, 2026',
+      time: '8:00 AM',
+      category: 'Volunteering',
+      location: 'Downtown Square - Farmers Markets'
+    },
+    {
+      title: 'Library Reading Time',
+      date: 'February 10, 2026',
+      time: '10:30 AM',
+      category: 'Education',
+      location: 'Public Library - Children’s Corner'
+    },
+    {
+      title: 'Pickup Soccer',
+      date: 'February 21, 2026',
+      time: '2:00 PM',
+      category: 'Recreation',
+      location: 'Heritage Park - North Soccer Field'
+    },
+    {
+      title: 'Pickup Basketball',
+      date: 'February 3, 2026',
+      time: '6:00 PM',
+      category: 'Recreation',
+      location: 'Community Rec Center - West Court'
     }
   ];
 
