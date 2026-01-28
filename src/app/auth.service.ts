e74345ae644e58827c2f30a275db45b973179cf6
Zephan Ofori <ofori.zephan@gmail.com>
2026-01-27 21:20:44 -0600

    removing comments

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -16,27 +16,21 @@ export class AuthService {
   username$ = this.username.asObservable();
 
   constructor(private http: HttpClient) {
-    // auto login if token exists
     const token = localStorage.getItem('token');
     if (token) {
       this.loggedIn.next(true);
     }
   }
 
-  /* ========================
-     SIGN UP
-  ======================== */
   signup(data: {
     name: string;
     email: string;
     password: string;
     community: string;
   }) {
-    // call backend signup
     return this.http.post<{ token: string; name: string }>(`${this.apiUrl}/signup`, data)
       .pipe(
         tap(res => {
-          // automatically log in the user
           localStorage.setItem('token', res.token);
           this.loggedIn.next(true);
           this.username.next(res.name);
@@ -44,9 +38,7 @@ export class AuthService {
       );
   }
 
-  /* ========================
-     LOGIN
-  ======================== */
+
   login(email: string, password: string) {
     return this.http
       .post<{ token: string; name: string }>(`${this.apiUrl}/login`, {
@@ -62,9 +54,7 @@ export class AuthService {
       );
   }
 
-  /* ========================
-     LOGOUT
-  ======================== */
+
   logout() {
     localStorage.removeItem('token');
     this.loggedIn.next(false);
