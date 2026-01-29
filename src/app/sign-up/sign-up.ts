893e858229d3bc58a94a656ed55233e2af49f594
Zephan Ofori <ofori.zephan@gmail.com>
2026-01-28 19:10:26 -0600

    sign-up logic

--- src/app/sign-up/sign-up.ts
+++ src/app/sign-up/sign-up.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
@@ -7,22 +7,25 @@ import {
   AbstractControl
 } from '@angular/forms';
 import { Router } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, NgIf],
   templateUrl: './sign-up.html',
   styleUrls: ['../sign-in/sign-in.css']
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
 
   signUpForm: FormGroup;
   showSuccess = false;
   errorMsg = '';
   loading = false;
+
+  restrictedCommunities = ['georgetown', 'austin'];
+  communityWarning = '';
 
   constructor(
     private fb: FormBuilder,
@@ -38,6 +41,20 @@ export class SignUpComponent {
     }, { validators: this.passwordsMatch });
   }
 
+  ngOnInit(): void {
+    this.community.valueChanges.subscribe(value => {
+      if (this.restrictedCommunities.includes(value)) {
+        this.communityWarning =
+          `This community's resources are online  yet. Please await our message for a new community welcome. Any questions contact our office for assistance`;
+
+        this.community.setErrors({ restricted: true });
+      } else {
+        this.communityWarning = '';
+        this.community.setErrors(null);
+      }
+    });
+  }
+
   get name() { return this.signUpForm.get('name')!; }
   get email() { return this.signUpForm.get('email')!; }
   get password() { return this.signUpForm.get('password')!; }
@@ -47,7 +64,6 @@ export class SignUpComponent {
   passwordsMatch(group: AbstractControl) {
     const pass = group.get('password')?.value;
     const confirm = group.get('confirmPassword')?.value;
-
     return pass === confirm ? null : { mismatch: true };
   }
 
@@ -63,33 +79,29 @@ export class SignUpComponent {
     if (this.signUpForm.invalid) {
       this.signUpForm.markAllAsTouched();
       return;
-    } else {
-      this.loading = true;
-      this.errorMsg = '';
-
-      const data = {
-        name: this.name.value,
-        email: this.email.value,
-        password: this.password.value,
-        community: this.community.value
-      };
-
-      this.authService.signup(data).subscribe({
-        next: () => {
-          this.authService.login(this.email.value, this.password.value)
-          this.loading = false;
-          this.showSuccess = true;
-        },
-        error: err => {
-          this.loading = false;
-          this.errorMsg = err.error?.message || 'Signup failed';
-          console.log(this.errorMsg)
-        }
-      });
-
     }
 
+    this.loading = true;
+    this.errorMsg = '';
 
+    const data = {
+      name: this.name.value,
+      email: this.email.value,
+      password: this.password.value,
+      community: this.community.value
+    };
+
+    this.authService.signup(data).subscribe({
+      next: () => {
+        this.authService.login(this.email.value, this.password.value);
+        this.loading = false;
+        this.showSuccess = true;
+      },
+      error: err => {
+        this.loading = false;
+        this.errorMsg = err.error?.message || 'Signup failed';
+      }
+    });
   }
 
   goToResources(): void {
