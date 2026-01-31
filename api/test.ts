4cab6aedf0c66c16e02ad69b48a1679f585b4da5
Zephan Ofori <ofori.zephan@gmail.com>
2026-01-30 20:55:53 -0600

    even mo

--- api/test.ts
+++ api/test.ts
@@ -1,10 +1,10 @@
-const OpenAI = require("openai");
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+// const OpenAI = require("openai");
+// const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 
 module.exports = (req, res) => {
   console.log("Test function called");
-  console.log('whats the key', openai);
-  console.log('twice', process.env.OPENAI_API_KEY);
+  // console.log('whats the key', openai);
+  // console.log('twice', process.env.OPENAI_API_KEY);
   res.status(200).json({ message: "Serverless working!" });
 };
