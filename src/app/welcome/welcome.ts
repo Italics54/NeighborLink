1d9eb34208194aaf39338ce8947d5ed4d6783fbb
Zephan Ofori <ofori.zephan@gmail.com>
2026-03-14 19:24:55 -0500

    adding judges section

--- src/app/welcome/welcome.ts
+++ src/app/welcome/welcome.ts
@@ -4,7 +4,7 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 interface OnboardingStep {
   title: string;
-  text: string;
+  text: Array<string>;
   media: string;
   type: 'gif' | 'video';
 }
@@ -28,28 +28,40 @@ export class Welcome implements OnInit {
   steps: OnboardingStep[] = [
     {
       title: 'Search Resources',
-      text: 'Quickly find services and resources near you.',
+      text: ['Quickly find services and resources near you.'],
       media: '/assets/onboarding/resources.mp4',
       type: 'video'
     },
     {
       title: 'Discover Events',
-      text: 'Find local community events.',
+      text: ['Find local community events.'],
       media: '/assets/onboarding/calendar.mp4',
       type: 'video'
     },
     {
       title: 'AI Recommendations',
-      text: 'Get smart suggestions anf help from our Neighborly AI, NeighborBot.',
+      text: ['Get smart suggestions anf help from our Neighborly AI, NeighborBot.'],
       media: '/assets/onboarding/chatbot.mp4',
       type: 'video'
     },  
     {
       title: 'Submit a Resource',
-      text:  `Don't want to gatekeep? Submit a resource to help our people in your community`,
+      text:  [`Don't want to gatekeep? Submit a resource to help our people in your community`],
       media: '/assets/onboarding/submit.mp4',
       type: 'video'
     },
+    {
+      title: 'TSA JUDGES',
+      text: [
+      '1. Go straight to Sign In',
+      '2. To enter the website, here are your credentials: ',
+      'Email: tsajudges@neighborlink.com ',
+      'Password: TSATSA123! ',
+      '3. You are free to use the Sign Up option to test functionality',
+      '4. Also please observe our resources tab with all the materials we utilized'],
+     media: '/assets/onboarding/judges.mp4',
+      type: 'video'
+    }
   ];
 
   ngOnInit(): void {
