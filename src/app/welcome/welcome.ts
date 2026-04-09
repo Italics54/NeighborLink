fecb2dcd1b5a0369998cf61b9cc664feddcbfe4d
Zephan Ofori <ofori.zephan@gmail.com>
2026-04-09 08:02:52 -0500

    fixing start tour so that it will work again for judges

--- src/app/welcome/welcome.ts
+++ src/app/welcome/welcome.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-
+import { AuthService } from '../auth.service';
 
 interface OnboardingStep {
   title: string;
@@ -14,54 +14,58 @@ interface OnboardingStep {
   standalone: true,
   templateUrl: './welcome.html',
   styleUrls: ['./welcome.css'],
-  imports: [CommonModule]
+  imports: [CommonModule],
 })
-
 export class Welcome implements OnInit {
-
   @ViewChild('demoVideo') demoVideo!: ElementRef<HTMLVideoElement>;
-
 
   showModal = false;
   currentStep = 0;
+
+  constructor(private authService: AuthService) {}
+
+  loading: boolean = true;
+  showSuccess: boolean = true;
+  errorMsg: string = '';
 
   steps: OnboardingStep[] = [
     {
       title: 'Search Resources',
       text: ['Quickly find services and resources near you.'],
       media: '/assets/onboarding/resources.mp4',
-      type: 'video'
+      type: 'video',
     },
     {
       title: 'Discover Events',
       text: ['Find local community events.'],
       media: '/assets/onboarding/calendar.mp4',
-      type: 'video'
+      type: 'video',
     },
     {
       title: 'AI Recommendations',
       text: ['Get smart suggestions anf help from our Neighborly AI, NeighborBot.'],
       media: '/assets/onboarding/chatbot.mp4',
-      type: 'video'
-    },  
+      type: 'video',
+    },
     {
       title: 'Submit a Resource',
-      text:  [`Don't want to gatekeep? Submit a resource to help people in your community`],
+      text: [`Don't want to gatekeep? Submit a resource to help people in your community`],
       media: '/assets/onboarding/submit.mp4',
-      type: 'video'
+      type: 'video',
     },
     {
       title: 'TSA JUDGES',
       text: [
-      '1. Go straight to Sign In',
-      '2. To enter the website, here are your credentials: ',
-      'Email: tsajudges@neighborlink.com ',
-      'Password: TSATSA123! ',
-      '3. You are free to use the Sign Up option to test functionality',
-      '4. Also please observe our resources tab with all the materials we utilized'],
-     media: '/assets/onboarding/judges.mp4',
-      type: 'video'
-    }
+        '1. Go straight to Sign In',
+        '2. To enter the website, here are your credentials: ',
+        'Email: tsajudges@neighborlink.com ',
+        'Password: TSATSA123! ',
+        '3. You are free to use the Sign Up option to test functionality',
+        '4. Also please observe our resources tab with all the materials we utilized',
+      ],
+      media: '/assets/onboarding/judges.mp4',
+      type: 'video',
+    },
   ];
 
   ngOnInit(): void {
@@ -92,6 +96,18 @@ export class Welcome implements OnInit {
   }
 
   closeModal(): void {
+    this.authService.login('tsajudges@neighborlink.com', 'TSATSA123!').subscribe({
+      next: () => {
+        this.loading = false;
+        this.showSuccess = true;
+        window.location.reload()
+      },
+      error: (err) => {
+        this.loading = false;
+        this.errorMsg = err.error?.message || 'Login failed';
+      },
+    });
+    
     this.showModal = false;
     localStorage.setItem('seenIntroModal', 'true');
   }
